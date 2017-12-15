c5af143fd4045f48d9b476712c53a00329d5ed75
James Donlan <jacketlizard@live.com>
2017-12-15 10:46:03 -0800

    Third spec met, pizzas now have a size in addition to toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // back end
-function Pizza(pizzaToppings) {
+function Pizza(pizzaToppings, pizzaSize) {
   this.toppings = pizzaToppings;
+  this.size = pizzaSize;
 }
 
 
@@ -8,14 +9,15 @@ function Pizza(pizzaToppings) {
 $(document).ready(function() {
   $("form#pizzaform").submit(function(event) {
     event.preventDefault();
+    var pizzaSize = $("input:radio[name=size]:checked").val();
     var pizzaToppings = [];
     $("input:checkbox[name=toppings]:checked").each(function(){
       pizzaToppings.push($(this).val());
     });
-    var yourPizza = new Pizza(pizzaToppings)
+    var yourPizza = new Pizza(pizzaToppings, pizzaSize)
         $("#results").empty();
         $("#order").show();
-        $("#results").append("Enjoy your pizza!");
+        $("#results").append("Enjoy your " + yourPizza.size.toString() + " pizza!");
         $("#toppingsreadout").append("<li>" + yourPizza.toppings.join("</li> <li>") + "</li>")
   });
 });
